5377e1a6abab8fca64a4ac6259a6375630afe028
Renata Mantovani <renatacmantovani@gmail.com>
2024-06-24 21:59:44 -0300

    att modularizando acessibilidade js

--- javascript/acessibility.js
+++ javascript/acessibility.js
@@ -1,3 +1,7 @@
+import { adjustFontSize } from './modules/fontSize.js';
+import { toggleTheme, setTheme } from './modules/themeToggle.js';
+import { startReading, pauseReading, resumeReading, setButtonState } from './modules/textReader.js';
+
 document.addEventListener('DOMContentLoaded', function() {
     const increaseFontSizeBtn = document.getElementById('acessibilidade_icon_aumentar');
     const decreaseFontSizeBtn = document.getElementById('acessibilidade_icon_diminuir');
@@ -6,15 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const pauseTextBtn = document.getElementById('pause-text');
     const stoppedTextBtn = document.getElementById('stop-text');
 
-    function adjustFontSize(multiplier) {
-        const elements = document.querySelectorAll('body *'); // Seleciona todos os elementos dentro do body
-        elements.forEach(el => {
-            const currentFontSize = window.getComputedStyle(el).fontSize;
-            const currentSize = parseFloat(currentFontSize);
-            el.style.fontSize = `${currentSize * multiplier}px`;
-        });
-    }
-    
     // Adiciona os eventos de clique aos botões
     increaseFontSizeBtn.addEventListener('click', () => {
         adjustFontSize(1.1); // Aumenta o tamanho da fonte em 10%
@@ -24,94 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
         adjustFontSize(0.9); // Diminui o tamanho da fonte em 10%
     });
 
-    // Função para alternar entre temas claro e escuro
-    let isLightTheme = localStorage.getItem('isLightTheme') === 'true'; // Recupera o estado do tema do localStorage
-    setTheme(isLightTheme); // Define o tema inicial com base no valor recuperado
-
-    toggleHighContrastBtn.addEventListener('click', function() {
-        isLightTheme = !isLightTheme; // Alterna o estado do tema
-        setTheme(isLightTheme); // Define o tema com base no novo estado
-        localStorage.setItem('isLightTheme', isLightTheme); // Salva o estado do tema no localStorage
-    });
-
-    // Função para definir o tema
-    function setTheme(isLight) {
-        if (isLight) {
-            document.body.classList.add('light-theme');
-        } else {
-            document.body.classList.remove('light-theme');
-        }
-    }
-
-    let utterance = null;
-    let isReading = false;
-    let isPaused = false;
-
-    function startReading() {
-        if (!isReading) {
-            let textContent = document.body.innerText;
-
-            // Adiciona o alt das imagens ao texto
-            const images = document.getElementsByTagName('img');
-            for (let img of images) {
-                if (img.alt) {
-                    textContent += ' ' + img.alt;
-                }
-            }
-
-            utterance = new SpeechSynthesisUtterance(textContent);
-            utterance.lang = 'pt-BR';
-
-            utterance.onstart = function() {
-                isReading = true;
-                isPaused = false;
-                setButtonState('pause');
-            };
-
-            utterance.onend = function() {
-                isReading = false;
-                isPaused = false;
-                setButtonState('play');
-            };
-
-            window.speechSynthesis.speak(utterance);
-        }
-    }
-
-    function pauseReading() {
-        if (isReading && !isPaused) {
-            window.speechSynthesis.pause();
-            isPaused = true;
-            setButtonState('stopped');
-        }
-    }
-
-    function resumeReading() {
-        if (isPaused) {
-            window.speechSynthesis.resume();
-            isPaused = false;
-            setButtonState('pause');
-        }
-    }
-
-    function setButtonState(state) {
-        if (state === 'play') {
-            readTextBtn.querySelector('img').src = '/images/acessibilidade_audio.png';
-            readTextBtn.classList.remove('hidden');
-            pauseTextBtn.classList.add('hidden');
-            stoppedTextBtn.classList.add('hidden');
-        } else if (state === 'pause') {
-            readTextBtn.querySelector('img').src = 'images/acessibilidade_audio_pausa.png';
-            readTextBtn.classList.add('hidden');
-            pauseTextBtn.classList.remove('hidden');
-            stoppedTextBtn.classList.add('hidden');
-        } else if (state === 'stopped') {
-            readTextBtn.querySelector('img').src = 'images/acessibilidade_audio_stopped.png';
-            readTextBtn.classList.add('hidden');
-            pauseTextBtn.classList.add('hidden');
-            stoppedTextBtn.classList.remove('hidden');
-        }
-    }
+    toggleHighContrastBtn.addEventListener('click', toggleTheme);
 
     readTextBtn.addEventListener('click', startReading);
     pauseTextBtn.addEventListener('click', pauseReading);
@@ -119,11 +27,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Inicializa os botões no estado inicial (a iniciar)
     setButtonState('play');
-
-    // Evento para finalizar o áudio ao recarregar a página
-    window.addEventListener('beforeunload', function() {
-        if (isReading) {
-            window.speechSynthesis.cancel();
-        }
-    });
 });
